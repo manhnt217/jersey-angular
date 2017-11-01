6574b73a1c0be3437f5af22a352c14c7aa7c1425
ManhNT8 <ManhNT8@fsoft.com.vn>
2017-11-01 14:39:57 +0700

    Change log level of trace message

--- src/main/java/com/example/jersey_sample/gitlab/WebHook.java
+++ src/main/java/com/example/jersey_sample/gitlab/WebHook.java
@@ -8,12 +8,11 @@ import static com.example.jersey_sample.Log.GITLAB_LOG;
 /**
  * @author manhnt
  */
-@SuppressWarnings("ALL")
 public class WebHook {
 
 	@POST
 	@Path("/push")
 	public void pushEventHook(String body) {
-		GITLAB_LOG.trace("Body: " + body);
+		GITLAB_LOG.warn("Body: " + body);
 	}
 }
